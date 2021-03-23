f3b5d541d827783d7aa1118f3371ec996c92e7ab
NekoSilverfox <weidufox@gmail.com>
2021-03-23 18:58:43 +0000

    增加 讲解笔记

--- 032 - 虚拟内存 - Unix/Client.c
+++ 032 - 虚拟内存 - Unix/Client.c
@@ -6,6 +6,8 @@
 #include <sys/shm.h>
 #include "shmem.h"
 
+// https://blog.csdn.net/guoping16/article/details/6584043
+// Семафор
 void main(void)
 {
 	Message* msgptr;
@@ -13,81 +15,106 @@ void main(void)
 	int shmid, semid;
 
 	//получение ключа. Один и тот же ключ можно использовать как для семафора, так и для разделяемой памяти
+	// 获取密钥。 相同的键可用于信号量和共享内存
 	if((key = ftok("test.txt", 'A')) < 0)
 	{
 		printf("Client: can't get a key\n");
 		exit(-1);
 	}
 
-	//получение доступа к разделяемой памяти
+	// получение доступа к разделяемой памяти
+	// 访问共享内存
 	if((shmid = shmget(key, sizeof(Message), 0)) < 0)
 	{
 		printf("Client: access denied\n");
 		exit(-1);
 	}
 
-	//присоединение разделяемой памяти
+	// присоединение разделяемой памяти
 	if((msgptr = (Message*)shmat(shmid, 0, 0)) < 0)
 	{
 		printf("Client : error of joining\n");
 		exit(-1);
 	}
 
-	//получение доступа к семафору
+	// получение доступа к семафору
+	// 获取信号体的访问权限
 	if((semid = semget(key, 2, PERM)) < 0)
 	{
 		printf("Client: access denied\n");
 		exit(-1);
 	}
 
-	//Заблокировка разделяемой памяти
-	if(semop(semid, &mem_lock[0], 2) < 0)
+	// Заблокировка разделяемой памяти
+	// 锁定共享内存
+	if(semop(semid, &mem_lock[0], 2) < 0)  // 其绝对值（абсолютная ценность）表示等待使用该资源的进程个数
 	{
 		printf("Client : can't execute a operation\n");
 		exit(-1);
 	}
 
-	//уведомление сервера о начале работы
+	// уведомление сервера о начале работы
+	// 服务器启动通知
 	if(semop(semid, &proc_start[0], 1) < 0)
 	{
 		printf("Client : can't execute a operation\n");
 		exit(-1);
 	}
 
-	//Запишем в разделяемую память сообщение
+	// Запишем в разделяемую память сообщение
+	// 信息写入共享内存中
 	printf("Client: enter name of file:\n  ");
 	char getname[MAXBUFF];
 	fgets(getname,MAXBUFF,stdin);
 	sprintf(msgptr->buff,"%s",getname);
 
-	//Освободим разделяемую память
+	// Освободим разделяемую память
+	// 释放共享内存
 	if(semop(semid, &mem_unlock[0], 1) < 0)
 	{
 		printf("Client: can't execute a operation\n");
 		exit(-1);
 	}
 
-	//Ждем, пока сервер в свою очередь не освободит разделяемую память
+	// Ждем, пока сервер в свою очередь не освободит разделяемую память
+	// 等待服务器依次释放共享内存
 	if(semop(semid, &mem_lock[0], 2) < 0)
 	{
 		printf("Client: can't execute a operation\n");
 		exit(-1);
 	}
 
-	//чтение сообщения из разделяемой памяти
+	// чтение сообщения из разделяемой памяти
+	// 从共享内存中读取消息
 	printf("Client: read message from file:\n  %s", msgptr->buff);
 
-	//освобождение разделяемой памяти
+#if 0
+	// освобождение разделяемой памяти
+	// 释放共享内存
 	if(semop(semid, &mem_unlock[0], 1) < 0)
 	{
 		printf("Client: can't execute a operation\n");
 		exit(-1);
 	}
-	
-	//отключение от области разделяемой памяти
+#endif
+
+	// отключение от области разделяемой памяти
+	// 从共享存储区分离
 	if(shmdt(msgptr) < 0)
 	{
 		printf("Client: error\n");
 		exit(-1);
+	}
+
+	// Удалим созданные объекты IPC
+	if(shmctl(shmid, IPC_RMID, 0)<0)
+	{
+		printf("Невозможно удалить область\n");
+		exit(1);
+	}
+	if(semctl(semid, 0, IPC_RMID)<0)
+	{
+		printf("Невозможно удалить семафор\n");
+		exit(1);
 	}
 }
--- 032 - 虚拟内存 - Unix/Server.c
+++ 032 - 虚拟内存 - Unix/Server.c
@@ -17,21 +17,24 @@ void main(void)
 	key_t key;
 
 	//получение ключа как для семафора так и для разделяемой памяти
+	// 获取信号量和共享内存的密钥
 	if((key = ftok("test.txt", 'A')) < 0)
 	{
 		printf("Server: can't get a key\n");
 		exit(-1);
 	}
 
-	//создание области разделяемой памяти
-	if((shmid = shmget(key, sizeof(Message), PERM | IPC_CREAT)) < 0)
+	// создание области разделяемой памяти
+	// 创建一个共享存储区
+	if((shmid = shmget(key, sizeof(Message), PERM | IPC_CREAT)) < 0)  // Inter-Process Communication
 	{
 		printf("Server: can't create an area\n");
 		exit(-1);
 	}
 	printf("Server: area is created\n");
 
-	//присоединение области
+	// присоединение области
+	// 加入该地区
 	if((msgptr = (Message*)shmat(shmid, 0, 0)) < 0)
 	{
 		printf("Server: error of joining\n");
@@ -39,9 +42,9 @@ void main(void)
 	}
 	printf("Server: area is joined\n");
 
-	//создание группы из 2 семафоров
-	//1 – для синхронизации работы с разделяемой памятью
-	//2 – для синхронизации выполнения процессов
+	//создание группы из 2 семафоров 创建一组2个信号量
+	//1 – для синхронизации работы с разделяемой памятью 与共享内存同步工作
+	//2 – для синхронизации выполнения процессов 同步流程的执行
 	if((semid = semget(key, 2, PERM | IPC_CREAT)) < 0)
 	{
 		printf("Server: can't create a semaphore\n");
@@ -51,21 +54,25 @@ void main(void)
 
 	while(1)
 	{
-		//ожидание начала работы клиента
+		// ожидание начала работы клиента
+		// 等待客户开始工作
+		// 对信号量集标识符为semid中的一个或多个信号量进行P操作或V操作
 		if(semop(semid, &proc_wait[0], 1) < 0)
 		{
 			printf("Server: execution complete\n");
 			exit(-1);
 		}
 
-		//ожидание завершения работы клиента с разделяемой памятью
+		// ожидание завершения работы клиента с разделяемой памятью
+		// 等待客户端使用共享内存关闭
 		if(semop(semid, &mem_lock[0], 2) < 0)
 		{
 			printf("Server: can't execute a operation\n");
 			exit(-1);
 		}
 
-		//вывод сообщения, записанного клиентом в разделяемую память
+		// вывод сообщения, записанного клиентом в разделяемую память
+		// 将客户端写入的消息输出到共享内存
 		printf("Server: read name of file\n  %s", msgptr->buff);
 		len=strlen(msgptr->buff);
 		msgptr->buff[len-1]='\0';
@@ -74,7 +81,8 @@ void main(void)
 			sprintf(msgptr->buff,"%s","Server: no file!\n");
 			printf("Server: can not open file name\n");
 
-			//освобождение разделяемой памяти
+			// освобождение разделяемой памяти
+			// 释放共享内存
 			semop(semid, &mem_unlock[0], 1);
 		}
 		else 
@@ -82,7 +90,8 @@ void main(void)
 			printf("Server: file is opened!\n");
 			while(read(fd,gettext,MAXBUFF)>0)
 				printf("Server: read message from file:\n  %s",gettext);
-			//запись сообщения в разделяемую память
+			// запись сообщения в разделяемую память
+			// 将消息写入共享内存
 			sprintf(msgptr->buff,"%s",gettext);
 			close(fd);
 		}
